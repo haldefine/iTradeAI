14c4cdbb7dbd8e1ec9a70e818bd3c2802e2f9fc4
Daniil Tereshchenko <tereshchenkodwork@gmail.com>
2024-03-24 21:27:47 +0200

    feat(dataset): implemented HDF5 format for datasets for pairs and dates

--- record_dataset.py
+++ record_dataset.py
@@ -1,18 +1,35 @@
 import h5py
 import numpy as np
 
-hdf5_file_path = 'datasets/your_file.h5'
+def addData(pair, date, data):
+    with h5py.File('datasets/data.h5', 'a') as file:
+        if pair not in file:
+            pair_group = file.create_group(pair)
+        else:
+            pair_group = file[pair]
 
-new_data = np.random.rand(100)
+        if date not in pair_group:
+            pair_group.create_dataset(date, data=data)
+        else:
+            dataset = pair_group[date]
+            new_size = dataset.shape[0] + data.shape[0]
+            dataset.resize(new_size, axis=0)
+            dataset[-data.shape[0]:] = data
 
-with h5py.File(hdf5_file_path, 'a') as file:
-    if 'your_dataset_name' in file:
-        dataset = file['your_dataset_name']
-        new_size = dataset.shape[0] + new_data.shape[0]
-        dataset.resize(new_size, axis=0)
-        dataset[-new_data.shape[0]:] = new_data
-    else:
-        dataset = file.create_dataset('your_dataset_name', data=new_data, maxshape=(None,))
 
-    read_data = dataset[:]
+# addData('BTC-USDT', '06-01-2021', np.random.rand(2))
+
+def showData(pair, date):
+    with h5py.File('datasets/data.h5', 'r') as file:
+        if pair in file:
+            pair_group = file[pair]
+            if date in pair_group:
+                data = pair_group[date][:]
+                print(f'{pair} - {date}: {data}')
+            else:
+                print(f"No data for date {date} in pair {pair}")
+        else:
+            print(f"No data for pair {pair}")
+
+# showData('BTC-USDT', '06-01-2021')
 
